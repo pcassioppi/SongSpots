63009630a35098a60ddd6d5c2472a87e2af802fe
pcassioppi <pjcassioppi@gmail.com>
2021-02-08 18:00:20 -0600

    added react submodule

--- songspots/songspots/settings.py
+++ songspots/songspots/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'songspots.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'NAME': 'SongSpots',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'testdb.cqkvg3uj1j8v.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
